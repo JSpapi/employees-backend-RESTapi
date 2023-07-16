b3108b51537f69cffb59edce4c464eba1eabbb23
JSpapi <testing@gmail.com>
2023-07-16 14:33:47 +0500

    refactor api url

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use("/api/user", usersRouter);
-app.use("/api/employees", employeesRouter);
+app.use("/user", usersRouter);
+app.use("/employees", employeesRouter);
 
 module.exports = app;
